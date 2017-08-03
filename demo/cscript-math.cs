33a9b6dc48326acba3cf717cb15bcedc78e5d603
Oliver Cordes <ocordes@astro.uni-bonn.de>
2017-08-03 20:06:21 +0200

    - add new math demo

---
+++ demo/cscript-math.cs
@@ -0,0 +1,32 @@
+#!/Users/ocordes/git/cscript/build/cscript -f
+
+#
+
+# start
+# blah
+#end
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
+
+int main( int argc, char* argv[] )
+{
+  int i;
+  double d;
+
+  /*printf( "Hello, this is a c-program running as a pseudo script!\n" );*/
+  puts( "Hello, this is a c-program running as a pseudo script!" );
+  for (i=1;i<argc;++i)
+    printf( "%i: %s\n", i, argv[i] );
+
+  /*exit ( 20 ); */
+
+  if ( argc > 1 )
+  {
+    d = atof( argv[1] );
+    printf( "sin: %f\n", sin( d ) );
+  }
+
+  return 0;
+}
